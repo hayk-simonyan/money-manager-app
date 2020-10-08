602551b6e97e814a8cbf69def27e2d5dc45290a2
Hayk <hsimonyan09@gmail.com>
2020-10-08 10:45:21 +0400

    convert auth actions into typescript

---
+++ src/redux/auth/auth.actions.ts
@@ -0,0 +1,179 @@
+import axios from 'axios';
+import setAuthToken from '../../utils/setAuthToken';
+import {
+  SIGNUP_SUCCESS,
+  SIGNUP_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  SIGNIN_SUCCESS,
+  SIGNIN_FAIL,
+  LOGOUT,
+  PUT_EMAIL,
+  PUT_EMAIL_ERROR,
+  PUT_PASSWORD,
+  PUT_PASSWORD_ERROR,
+} from './auth.types';
+
+import { getAccounts } from '../accounts/account.actions';
+import { getCategories } from '../categories/category.actions';
+import { getRecords } from '../records/record.actions';
+import { setAlert } from '../alerts/alert.actions';
+
+export const signup = (name: string, email: string, password: string) => async (
+  dispatch: any
+) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const body = JSON.stringify({ name, email, password });
+
+  try {
+    const res = await axios.post('http://localhost:5000/users', body, config);
+
+    dispatch({
+      type: SIGNUP_SUCCESS,
+      payload: res.data,
+    });
+
+    dispatch(loadUser());
+
+    dispatch(setAlert('Welcome to Money Manager', 'success'));
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((err: any) => dispatch(setAlert(err.msg, 'danger')));
+    }
+
+    dispatch({
+      type: SIGNUP_FAIL,
+    });
+  }
+};
+
+export const signin = (email: string, password: string) => async (
+  dispatch: any
+) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post('http://localhost:5000/auth', body, config);
+
+    dispatch({
+      type: SIGNIN_SUCCESS,
+      payload: res.data,
+    });
+
+    dispatch(loadUser());
+
+    dispatch(setAlert('Welcome back', 'success'));
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((err: any) => dispatch(setAlert(err.msg, 'danger')));
+    }
+
+    dispatch({
+      type: SIGNIN_FAIL,
+    });
+  }
+};
+
+export const logout = () => (dispatch: any) => {
+  dispatch({ type: LOGOUT });
+  dispatch(setAlert('Logged you out', 'success'));
+};
+
+export const loadUser = () => async (dispatch: any) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get('http://localhost:5000/auth');
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+
+    dispatch(getAccounts());
+    dispatch(getCategories());
+    dispatch(getRecords());
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
+
+export const putEmail = (email: string) => async (dispatch: any) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const body = JSON.stringify({ email });
+
+  try {
+    const res = await axios.put(`/auth/email`, body, config);
+    console.log(res);
+
+    dispatch({
+      type: PUT_EMAIL,
+      payload: res.data,
+    });
+
+    dispatch(setAlert(res.data.msg, 'success'));
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((err: any) => dispatch(setAlert(err.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PUT_EMAIL_ERROR,
+    });
+  }
+};
+
+export const putPassword = (oldPassword: string, password: string) => async (
+  dispatch: any
+) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const body = JSON.stringify({ oldPassword, password });
+
+  try {
+    const res = await axios.put(`/auth/password`, body, config);
+
+    dispatch({
+      type: PUT_PASSWORD,
+      payload: res.data,
+    });
+
+    dispatch(setAlert(res.data.msg, 'success'));
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((err: any) => dispatch(setAlert(err.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PUT_PASSWORD_ERROR,
+    });
+  }
+};
---
+++ src/redux/auth/auth.reducers.ts
@@ -0,0 +1,67 @@
+import {
+  USER_LOADED,
+  AUTH_ERROR,
+  SIGNIN_SUCCESS,
+  SIGNIN_FAIL,
+  LOGOUT,
+  SIGNUP_SUCCESS,
+  SIGNUP_FAIL,
+  PUT_EMAIL,
+  PUT_EMAIL_ERROR,
+  PUT_PASSWORD,
+  PUT_PASSWORD_ERROR,
+} from './auth.types';
+
+const initialState = {
+  token: localStorage.getItem('token'),
+  isAuthenticated: null,
+  loading: true,
+  user: null,
+};
+
+export default function (
+  state = initialState,
+  action: { type: string; payload: any }
+) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case SIGNUP_SUCCESS:
+    case SIGNIN_SUCCESS:
+      localStorage.setItem('token', payload.token);
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false,
+      };
+    case PUT_PASSWORD:
+    case PUT_EMAIL:
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload,
+      };
+    case PUT_PASSWORD_ERROR:
+    case PUT_EMAIL_ERROR:
+      return {
+        ...state,
+        loading: false,
+      };
+    case SIGNUP_FAIL:
+    case AUTH_ERROR:
+    case SIGNIN_FAIL:
+    case LOGOUT:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
