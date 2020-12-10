0a592c9a3189877596bc0d04d192afb739352b2f
Hayk <hsimonyan09@gmail.com>
2020-12-10 15:27:02 +0400

    remove android with old dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,6 @@ import { Route, Redirect } from 'react-router-dom';
 import { IonReactRouter } from '@ionic/react-router';
 import { Plugins } from '@capacitor/core';
 
-import store from './redux/store';
-
-import Homepage from './pages/home-page/homepage';
-import NewRecordPage from './pages/new-record-page/new-record-page';
-import RecordsPage from './pages/records-page/records-page';
-import EditRecordPage from './pages/edit-record-page/edit-record-page';
-import AboutPage from './pages/about-page/about-page';
-import Menu from './components/menu/menu';
-import SignupPage from './pages/signup-page/signup-page';
-import SigninPage from './pages/signin-page/signin-page';
-import setAuthToken from './utils/setAuthToken';
-import PrivateRoute from './containers/private-route/private-route';
-import NewAccountPage from './pages/new-account-page/new-account-page';
-import EditAccountPage from './pages/edit-account-page/edit-account-page';
-import Alert from './containers/alert/alert';
-import CategoriesPage from './pages/categories-page/categories-page';
-import NewCategoryPage from './pages/new-category-page/new-category-page';
-import EditCategoryPage from './pages/edit-category-page/edit-category-page';
-
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
@@ -46,6 +27,25 @@ import { loadUser } from './redux/auth/auth.actions';
 import { Provider } from 'react-redux';
 import SettingsPage from './pages/settings-page/settings-page';
 import NetworkError from './pages/network-error/network-error';
+
+import store from './redux/store';
+
+import Homepage from './pages/home-page/homepage';
+import NewRecordPage from './pages/new-record-page/new-record-page';
+import RecordsPage from './pages/records-page/records-page';
+import EditRecordPage from './pages/edit-record-page/edit-record-page';
+import AboutPage from './pages/about-page/about-page';
+import Menu from './components/menu/menu';
+import SignupPage from './pages/signup-page/signup-page';
+import SigninPage from './pages/signin-page/signin-page';
+import setAuthToken from './utils/setAuthToken';
+import PrivateRoute from './containers/private-route/private-route';
+import NewAccountPage from './pages/new-account-page/new-account-page';
+import EditAccountPage from './pages/edit-account-page/edit-account-page';
+import Alert from './containers/alert/alert';
+import CategoriesPage from './pages/categories-page/categories-page';
+import NewCategoryPage from './pages/new-category-page/new-category-page';
+import EditCategoryPage from './pages/edit-category-page/edit-category-page';
 
 const { Network, StatusBar } = Plugins;
 
