08b65d148f3da6843c34b1c29cfb9b72bd2aba94
hayk77 <hsimonyan09@gmail.com>
2020-12-21 21:00:00 +0400

    add option to choose date by year

--- src/containers/month-picker/month-picker.tsx
+++ src/containers/month-picker/month-picker.tsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
-import { IonSelect, IonSelectOption } from '@ionic/react';
+import {
+  IonCol,
+  IonDatetime,
+  IonRow,
+  IonSelect,
+  IonSelectOption,
+} from '@ionic/react';
 
 import { connect } from 'react-redux';
 import { getRecords } from '../../redux/records/record.actions';
@@ -17,29 +23,44 @@ const MonthPicker: React.FC<Props> = ({ getRecords }) => {
 
   useEffect(() => {
     getRecords(year, month);
-  }, [month]);
+  }, [year, month]);
 
   return (
-    <IonSelect
-      value={month}
-      interface='action-sheet'
-      cancelText='Cancel'
-      okText='Ok'
-      onIonChange={(e: any) => setMonth(e.detail.value)}
-    >
-      <IonSelectOption value='00'>January</IonSelectOption>
-      <IonSelectOption value='01'>February</IonSelectOption>
-      <IonSelectOption value='02'>March</IonSelectOption>
-      <IonSelectOption value='03'>April</IonSelectOption>
-      <IonSelectOption value='04'>May</IonSelectOption>
-      <IonSelectOption value='05'>June</IonSelectOption>
-      <IonSelectOption value='06'>July</IonSelectOption>
-      <IonSelectOption value='07'>August</IonSelectOption>
-      <IonSelectOption value='08'>September</IonSelectOption>
-      <IonSelectOption value='09'>October</IonSelectOption>
-      <IonSelectOption value='10'>November</IonSelectOption>
-      <IonSelectOption value='11'>December</IonSelectOption>
-    </IonSelect>
+    <div>
+      <IonRow>
+        <IonCol>
+          <IonSelect
+            value={month}
+            interface='action-sheet'
+            cancelText='Cancel'
+            okText='Ok'
+            onIonChange={(e: any) => setMonth(e.detail.value)}
+          >
+            <IonSelectOption value='00'>January</IonSelectOption>
+            <IonSelectOption value='01'>February</IonSelectOption>
+            <IonSelectOption value='02'>March</IonSelectOption>
+            <IonSelectOption value='03'>April</IonSelectOption>
+            <IonSelectOption value='04'>May</IonSelectOption>
+            <IonSelectOption value='05'>June</IonSelectOption>
+            <IonSelectOption value='06'>July</IonSelectOption>
+            <IonSelectOption value='07'>August</IonSelectOption>
+            <IonSelectOption value='08'>September</IonSelectOption>
+            <IonSelectOption value='09'>October</IonSelectOption>
+            <IonSelectOption value='10'>November</IonSelectOption>
+            <IonSelectOption value='11'>December</IonSelectOption>
+          </IonSelect>
+        </IonCol>
+        <IonCol>
+          <IonDatetime
+            pickerFormat='YYYY'
+            displayFormat='YYYY'
+            value={year}
+            onIonChange={(e) => setYear(e.detail.value!)}
+            display-timezone='utc'
+          ></IonDatetime>
+        </IonCol>
+      </IonRow>
+    </div>
   );
 };
 
