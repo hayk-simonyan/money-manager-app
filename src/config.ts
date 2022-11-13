6d5e80c798595f5129019d9c1dc16ba16a0f911a
hayk-simonyan <hayksimonyan7@gmail.com>
2022-11-13 23:41:16 +0400

    server from cyclic.sh to render

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 const config = {
   backendUrl: isDevelopment
     ? 'http://localhost:5000'
-    : 'https://money-manager-api.cyclic.app',
+    : 'https://money-manager-api-72vr.onrender.com',
 };
 
 export default config;
